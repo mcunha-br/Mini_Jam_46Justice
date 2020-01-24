73deb262f86d70cf4c0c2bfd982bd902ac775336
TekhnoDevelop <tekhnosoftdevelop@gmail.com>
2020-01-24 10:54:11 -0300

    Update AI.cs

--- Assets/Scripts/Marcos/AI.cs
+++ Assets/Scripts/Marcos/AI.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public abstract class AI : MonoBehaviour {
 
+	private int[] _distancePatrol = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
     public abstract void Patrol ();
+
+    public virtual int GetNewDistancePatrol () {
+    	return _distancePatrol[Random.Range(0, _distancePatrol.Length)];
+    }
+
 
 }
