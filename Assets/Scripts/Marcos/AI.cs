d9166c514b73c3360aeefd864c97ee15174ce919
TekhnoDevelop <tekhnosoftdevelop@gmail.com>
2020-01-24 12:15:42 -0300

    AI Integration #2

--- Assets/Scripts/Marcos/AI.cs
+++ Assets/Scripts/Marcos/AI.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public abstract class AI : MonoBehaviour {
 
-	private int[] _distancePatrol = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	[HideInInspector] public bool inPatrolling;
 
-    public abstract void Patrol ();
+	private byte[] _distancePatrol = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	private float[] _secondsPatrolDir = {5, 6, 7, 8, 9};
+	private float speed = 1.2f;
 
-    public virtual int GetNewDistancePatrol () {
+    public abstract void Patrol (Vector2 dir);
+
+    public virtual byte GetNewDirectionPatrol () {
     	return _distancePatrol[Random.Range(0, _distancePatrol.Length)];
     }
 
+    public virtual float GetNewSecondPatrolDir () {
+    	return _secondsPatrolDir[Random.Range(0, _secondsPatrolDir.Length)];
+    }
 
+    public float GetSpeed () {return speed;}
+
+
+}
+
+public enum DirectionState {
+	LEFT, RIGHT
 }
--- Assets/Scripts/Marcos/Enemy.cs
+++ Assets/Scripts/Marcos/Enemy.cs
@@ -2,25 +2,118 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : AI {
+public class Enemy : AI, IEnemy {
+
+	[Header("Stats")]
+	public bool isDead = false;
+	public EnemyStats stats;
+
+	[Header("Axis State Patrol")]
+	public DirectionState directionState;
 
 	private const string playerTag = "Player";
+	private byte direction;
+	private float countdown = 0f;
+	private float turnSecondsDir;
 
     protected Enemy () {}
 
     private void Start()  {
-        
+    	SetupStats();
+    	SetupPatrol();
     }
 
     private void Update() {
-        
+    	PatrollingAI();
     }
 
-    public override void Patrol () {
+    private void PatrollingAI () {
+    	if(!isDead){
+	    	if(inPatrolling){
+	    		if(direction %2 == 0){
+					Patrol(Vector3.right);
+					directionState = DirectionState.RIGHT;
+				}else{
+					Patrol(Vector3.left);
+					directionState = DirectionState.LEFT;
+				}
+			}
+		}
+    }
 
+    private void SetupPatrol () {
+    	inPatrolling = true;
+    	direction = GetNewDirectionPatrol();
+    	turnSecondsDir = GetNewSecondPatrolDir();
+    }
+
+    private void SetupStats () {
+    	isDead = false;
+    	stats = new EnemyStats("Player", 100);
+    }
+
+    public override void Patrol (Vector2 dir) {
+    	transform.Translate((dir * GetSpeed()) * Time.deltaTime);
+    	countdown++;
+    	if(countdown >= turnSecondsDir * 100){
+    		countdown = 0f;
+    		inPatrolling = false;
+    		StartCoroutine(WaitPatrolling());
+    	}
+    }
+
+    private IEnumerator WaitPatrolling () {
+    	yield return new WaitForSeconds(2f);
+    	SetupPatrol();
+    	StopAllCoroutines();
+    }
+
+    public void Demage (int dmg) {
+    	if(stats.GetHealth() <= 0){
+    		Die();
+    	}
+    	stats.Demage(dmg);
+    }
+
+    public void Die () {
+    	isDead = true;
+    	countdown = 0f;
+    	stats.SetHealth(0);
     }
 
     public Enemy GetEnemy () {return this;}
     public string GetTargetTag () {return playerTag;}
 
+}
+
+[System.Serializable]
+public class EnemyStats {
+
+	private const int DEFAULT_HEALTH = 100;
+
+	public string name;
+	public int health;
+
+	public EnemyStats (string name = "", int health = DEFAULT_HEALTH) {
+		this.health = health;
+		this.name = name;
+		if(name == ""){
+			name = "Player";
+		}
+	}
+
+	public void Setup () {
+		SetHealth(DEFAULT_HEALTH);
+	}
+
+	public void Demage (int dmg) {health -= dmg;}
+
+	public void SetHealth (int health) {this.health = health;}
+	public int GetHealth () {return health;}
+
+}
+
+public interface IEnemy {
+	void Demage(int dmg);
+	void Die();
 }
