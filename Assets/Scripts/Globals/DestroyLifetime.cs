d41089ae7e1887e486031946acf51bc1fd54278d
TekhnoDevelop <tekhnosoftdevelop@gmail.com>
2020-01-24 16:52:59 -0300

    .

---
+++ Assets/Scripts/Globals/DestroyLifetime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLifetime : MonoBehaviour {
+
+    private DestroyLifetime () {}
+
+    public float lifetime = 5f;
+     
+    private void  Awake (){
+        Destroy(this, lifetime);
+    }
+
+    private void Start()  {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    private DestroyLifetime GetDestroyLifetime () {
+        return this;
+    }
+
+}
