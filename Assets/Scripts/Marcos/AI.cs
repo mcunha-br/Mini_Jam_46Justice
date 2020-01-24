1a668842be248bb313ed67fb1d15a19492869442
TekhnoDevelop <tekhnosoftdevelop@gmail.com>
2020-01-24 10:48:55 -0300

    AI Integration #1

---
+++ Assets/Scripts/Marcos/AI.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AI : MonoBehaviour {
+
+    public abstract void Patrol ();
+
+}
---
+++ Assets/Scripts/Marcos/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : AI {
+
+	private const string playerTag = "Player";
+
+    private Enemy () {}
+
+    private void Start()  {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    public override void Patrol () {
+    	
+    }
+
+    private Enemy GetEnemy () {return this;}
+    private string GetTargetTag () {return playerTag;}
+
+}
