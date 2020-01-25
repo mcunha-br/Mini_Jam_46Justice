4937af41918554b9ec6b96e7424440e557f5bea0
luizero00 <33324621+luizero00@users.noreply.github.com>
2020-01-25 09:53:12 -0300

    Player Shoot script created

---
+++ Assets/Scripts/luizero/PlayerBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+	public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
