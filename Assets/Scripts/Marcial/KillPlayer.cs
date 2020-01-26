597143aef8a1222a9d1fa703a5f21363a39957fe
Marcial Lincoln <marcial.snake@gmail.com>
2020-01-26 09:20:10 -0300

    KillPlayer quando cai no buraco

--- Assets/Scripts/Marcial/KillPlayer.cs
+++ Assets/Scripts/Marcial/KillPlayer.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
 public class KillPlayer : MonoBehaviour {
+
+    public int damage = 30;
     
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-            other.GetComponent<PlayerBehaviour>().Damage(30);
+            other.GetComponent<PlayerBehaviour>().Damage(damage);
         }
          
     }
